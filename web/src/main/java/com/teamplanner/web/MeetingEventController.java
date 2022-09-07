50ab1771865fd8e5bd27bad824053c044c53265c
Lalith Sai <69085473+Lalith-Sai@users.noreply.github.com>
2022-09-07 11:16:46 +0200

    Add endpoint to recieve meeting events

---
+++ web/src/main/java/com/teamplanner/web/MeetingEventController.java
@@ -0,0 +1,20 @@
+package com.teamplanner.web;
+
+import com.teamplanner.web.events.MeetingEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class MeetingEventController {
+    private static final Logger LOG
+            = LoggerFactory.getLogger(MeetingEventController.class);
+
+    @PostMapping("/receiveMeetingEvent")
+    void receiveMeetingEvent(@RequestBody MeetingEvent meetingEvent) {
+        LOG.info("Recieved {}", meetingEvent.getMeetingId());
+    }
+
+}
